1f96d4dd68358addaf2f960a15bb75a4250ce182
doug <drehnstrom@yahoo.com>
2021-02-07 08:59:12 -0500

    Changes

--- automation/redploy_all.sh
+++ automation/redploy_all.sh
@@ -9,5 +9,9 @@ docker push drehnstrom/events-api:v1.0
 docker build -t drehnstrom/events-web:v1.0 ../events-website
 docker push drehnstrom/events-web:v1.0
 
+# Recreate Website Docker Containers
+docker build -t drehnstrom/events-job:v1.0 ./database-initializer
+docker push drehnstrom/events-job:v1.0
+
 # Apply Kubernetes configuration
 kubectl apply -f ../kubernetes-configurations/
--- database-initializer/server.js
+++ database-initializer/server.js
@@ -3,17 +3,6 @@
 // Going to connect to MySQL database
 const mysql = require('mysql');
 
-const HOST = process.env.DBHOST ? process.env.DBHOST : "127.0.0.1";
-const USER = process.env.DBUSER ? process.env.DBUSER : "root";
-const PASSWORD = process.env.DBPASSWORD ? process.env.DBPASSWORD : "Letmein!";
-const DATABASE = process.env.DBDATABASE ? process.env.DBDATABASE : "events_db";
-
-const connection = mysql.createConnection({
-    host: HOST,
-    user: USER,
-    password: PASSWORD,
-});
-
 const create_table_sql = `CREATE TABLE events(
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
@@ -28,31 +17,69 @@ const add_record_sql = `INSERT INTO events (title, event_time, description, loca
 VALUES ('Pet Show', 'June 6 at Noon', 
 'Super-fun with furry friends!', 'Reston Dog Park');`
 
-connection.connect(function (err) {
-    if (err) {
-        console.error(err.message);
-    }
-    else {
-        console.log("Connected!");
-        // Ensure the database doesn't exist
-        connection.query("DROP DATABASE IF EXISTS events_db;", function (err, result) {
-            console.log("Dropped Database");
-            // Create the database
-            connection.query("CREATE DATABASE events_db;", function (err, result) {
-                // Switch to the database
-                connection.query("USE events_db;", function (err, result) {
-                    console.log("Switched Database");
-                    // Create the Table
-                    connection.query(create_table_sql, function (err, result) {
-                        console.log("Table created");
-                        // Add a Record
-                        connection.query(add_record_sql, function (err, result) {
-                            console.log("Record added");
-                            process.exit()
+function init_database() {
+    const HOST = process.env.DBHOST ? process.env.DBHOST : "127.0.0.1";
+    const USER = process.env.DBUSER ? process.env.DBUSER : "root";
+    const PASSWORD = process.env.DBPASSWORD ? process.env.DBPASSWORD : "Letmein!";
+    const connection = mysql.createConnection({
+        host: HOST,
+        user: USER,
+        password: PASSWORD,
+    });
+    connection.connect(function (err) {
+        if (err) {
+            console.error(err.message);
+        }
+        else {
+            console.log("Connected!");
+            // Ensure the database doesn't exist
+            connection.query("DROP DATABASE IF EXISTS events_db;", function (err, result) {
+                console.log("Dropped Database");
+                // Create the database
+                connection.query("CREATE DATABASE events_db;", function (err, result) {
+                    // Switch to the database
+                    connection.query("USE events_db;", function (err, result) {
+                        console.log("Switched Database");
+                        // Create the Table
+                        connection.query(create_table_sql, function (err, result) {
+                            console.log("Table created");
+                            // Add a Record
+                            connection.query(add_record_sql, function (err, result) {
+                                console.log("Record added");
+                                succeeded = true;
+
+                            });
                         });
                     });
                 });
             });
-        });
+        }
+
+    });
+}
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+let tries = 0;
+let succeeded = false;
+let sleep_time = 1;
+async function start() {
+    while (!succeeded && tries < 3) {
+        tries++;
+        console.log(sleep_time);
+        await sleep(sleep_time * 1000);
+        init_database();
+        sleep_time >= 256 ? sleep_time = sleep_time : sleep_time *= 2
+        console.log(new Date().toLocaleTimeString());
+        
     }
-});
+    console.log("Exiting");
+    process.exit();
+}
+
+start()
+
