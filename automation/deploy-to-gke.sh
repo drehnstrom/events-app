21be08e4dc0d7e7fc19df3cc703beb849af01e6c
drehnstrom <drehnstrom@users.noreply.github.com>
2021-02-08 10:50:06 -0500

    Update deploy-to-gke.sh

--- automation/deploy-to-gke.sh
+++ automation/deploy-to-gke.sh
@@ -4,20 +4,27 @@
 # each gcloud command. 
 
 # Enable the required Cloud Services
+echo "Enabling Compute Engine and Kubernetes APIs."
+echo "This will take a couple minutes..."
 gcloud services enable compute.googleapis.com container.googleapis.com
+
+
 # Create the Cluster. Make sure you have a default Project Set.
+echo "Creating Kubernetes cluster..."
 gcloud container clusters create events-cluster --zone us-central1-c
 
 # Connect to your Cluster. This set the kubectl context
 gcloud container clusters get-credentials events-cluster --zone us-central1-c
 
 # Need the database installed first using Helm
+echo "Deploying Database..."
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm install database-server bitnami/mariadb
 
 # Give the database a chance to start
-echo "Will sleep for a minute to let the database start. "
+echo "Will sleep for a minute to let the database start..."
 sleep 1m
 
 # Once the database is installed, then apply all the Kubernetes configuration
+echo "Deploying application..."
 kubectl apply -f ../kubernetes-configurations/
